8520d2784b639ceb3014b4c9fd6415f220137ced
Muthia Anashya Salam <118595769+muthiasalam@users.noreply.github.com>
2024-06-18 02:25:00 +0800

    add bookmark feature

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 const routes = require('./routes/index'); 
-const loadModel = require('./services/loadModel');
-const InputError = require('./exceptions/inputError');
 const firebaseAdmin = require('firebase-admin'); 
 const { admin } = require('./config/firebase'); 
 const path = require('path');
 
-
-const serviceAccountPath = "./path/to/firestore-access/json/hidden";
-
+const serviceAccountPath = './path/service/account/disembunyikan';
 
 if (!admin.apps.length) {
     admin.initializeApp({
@@ -23,8 +19,8 @@ const db = firebaseAdmin.firestore();
 const init = async () => {
     try {
         const server = Hapi.server({
-            port: process.env.PORT || 3000,
-        host: process.env.HOST, 
+            port: process.env.PORT || 5000,
+            host: process.env.HOST,
             routes: {
                 cors: {
                     origin: ['*'],
@@ -43,39 +39,15 @@ const init = async () => {
             require('@hapi/cookie')
         ]);
 
-        const model = await loadModel();
-        server.app.model = model;
+        
         server.app.db = db;
 
-       
         routes.forEach(route => {
             server.route(route);
         });
         console.log('Routes registered:', routes);
 
-        server.ext('onPreResponse', function (request, h) {
-            const response = request.response;
-
-            if (response instanceof InputError) {
-                const newResponse = h.response({
-                    status: 'fail',
-                    message: `${response.message} Silakan periksa input Anda.`
-                });
-                newResponse.code(response.output.statusCode);
-                return newResponse;
-            }
-
-            if (response.isBoom) {
-                const newResponse = h.response({
-                    status: 'fail',
-                    message: response.output.payload.message
-                });
-                newResponse.code(response.output.statusCode);
-                return newResponse;
-            }
-
-            return h.continue;
-        });
+        
 
         await server.start();
         console.log(`Server started at: ${server.info.uri}`);
